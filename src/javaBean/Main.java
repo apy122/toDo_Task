c69052f16f74739092bc410600b492cde2e9cf2e
apy122 <apy122@kali.apy122>
2024-12-04 20:42:37 +0100

    planning

--- src/javaBean/Main.java
+++ src/javaBean/Main.java
@@ -1,5 +1,12 @@
 package javaBean;
 
+import java.util.Scanner;
+
 public class Main {
+	public static void main (String[] args) {
+		int option;
+		Scanner scanner=new Scanner(System.in);
+
+	}
 
 }
--- src/test/testToDo.java
+++ src/test/testToDo.java
@@ -1,10 +1,36 @@
 package test;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class testToDo {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private ArrayList<String> tasks;
+	private Scanner scanner;
+	
+	public testToDo(ArrayList<String> tasks, Scanner scanner) {
+		super();
+		this.tasks = tasks;
+		this.scanner = scanner;
 	}
-
+	//Menu
+	
+	
+	//See Task
+	public void taskList() {
+		
+	}
+	
+	//Add Task
+	public void addTask() {
+		
+	}
+	
+	//task Done
+	public void taskDone() {
+		
+	}
+	
+	//Task delete
+	public void taskdelete() {
+		
+	}
 }
